40fa0106143e7e7f2173651fcd5a8cafcf0b885e
Your Dream <yourdream1375@gmail.com>
2018-05-13 14:51:28 +0300

    Added Bootstrap and default router performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/* 
+============================================= 
+======           Modules          =========== 
+============================================= 
+*/
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+
+
+
+/* 
+============================================= 
+=======           Routes          =========== 
+============================================= 
+*/
+const appRoutes: Routes = [
+
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot( appRoutes ),
   ],
   providers: [],
   bootstrap: [AppComponent]
